159fd76ed6698793bde722b93340716ee9193703
Sharmina Akhtar <sakht318@gmail.com>
2018-04-17 21:57:01 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :students, only: [:show, :new, :create, :edit, :update]
+  resources :school_classes, only: [:show, :new, :create, :edit, :update]
   # root 'welcome#index'
 
   # Example of regular route:
