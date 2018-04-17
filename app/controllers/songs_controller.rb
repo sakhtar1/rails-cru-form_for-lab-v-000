15984f49d386b29a6db749045eb9ec215eb3ba41
Sharmina Akhtar <sakht318@gmail.com>
2018-04-17 22:46:08 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,26 +16,20 @@ class SongsController < ApplicationController
 
     def create
       @song = Song.new
-      @song = Song.create(name: [:song] [:name], artist_id: @artist.id, genre_id: @genre.id)
+      @song = Song.create(name: params([:song][:name]), artist_id: @artist.id, genre_id: @genre.id)
       @song.save
       redirect_to song_path(@song)
     end
 
     def update
       @song = Song.find(params[:id])
-      @song.update(song_params(name: [:song][:name]))
+      @song.u(name: params([:song][:name]), artist_id: @artist.id, genre_id: @genre.id)
       redirect_to song_path(@song)
     end
 
     def edit
       @song = Song.find(params[:id])
     end
-
-    private
-
-      def song_params(*args)
-        params.require(:song).permit(*args)
-      end
 
   end
 
