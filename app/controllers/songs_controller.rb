adeeb179a5da336800c86bdff93867193cda719f
Sharmina Akhtar <sakht318@gmail.com>
2018-04-17 22:32:36 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,36 @@
 class SongsController < ApplicationController
 
+    def show
+      @song = Song.find(params[:id])
+    end
+
+    def new
+      @song = Song.new
+    end
+
+    def create
+      @song = Song.new
+      @song = Song.new(song_params(name: [:song][:name])
+      @song.save
+      redirect_to song_path(@song)
+    end
+
+    def update
+      @song = Song.find(params[:id])
+      @song.update(song_params(name: [:song][:name]))
+      redirect_to song_path(@song)
+    end
+
+    def edit
+      @song = Song.find(params[:id])
+    end
+
+    private
+
+      def song_params(*args)
+        params.require(:song).permit(*args)
+      end
+
+  end
+
 end
