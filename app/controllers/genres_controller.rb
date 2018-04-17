8d64ae4035e5139907a878ae1985b2f0140277e1
Sharmina Akhtar <sakht318@gmail.com>
2018-04-17 22:18:29 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -11,7 +11,7 @@ class GenresController < ApplicationController
   def create
     @genre = Genre.new
     @genre = Genre.new(genre_params(name: [:genre][:name])
-    @genre.save
+
     redirect_to genre_path(@genre)
   end
 
