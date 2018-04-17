9462383cbd72ae4c16e58fcfb83992185673af05
Sharmina Akhtar <sakht318@gmail.com>
2018-04-17 23:03:17 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,7 +14,7 @@ class SongsController < ApplicationController
 
     def create
       @song = Song.new
-      @song = Song.create(song_params(:name, :artist_id, :genre_id)
+      @song = Song.create(song_params(:name, :artist_id, :genre_id))
       redirect_to song_path(@song)
     end
 
