fdd3c18d604ce0531fbb22e01b412799dbb632cb
Sharmina Akhtar <sakht318@gmail.com>
2018-04-17 22:15:41 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,3 +1,34 @@
 class GenresController < ApplicationController
 
+  def show
+    @genre = Artist.find(params[:id])
+  end
+
+  def new
+    @genre = Artist.new
+  end
+
+  def create
+    @genre = Artist.new
+    @genre = Artist.new(genre_params(name: [:genre][:name], bio: [:genre][:bio]))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def update
+    @genre = Artist.find(params[:id])
+    @genre.update(genre_params(name: [:genre][:name], bio: [:genre][:bio]))
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  private
+
+    def genre_params(*args)
+      params.require(:genre).permit(*args)
+    end
+
 end
